348f6ec67f6a4cdefcc1d388152443dc21f50cdc
justine jose <justinejose@justines-MacBook-Air.local>
2022-04-09 16:50:51 +0530

    retrieve seached topics frpm wiki , saved as txt file, display in bot

--- actions/search_topic.py
+++ actions/search_topic.py
@@ -3,9 +3,15 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet, AllSlotsReset
+import wikipedia
 
+def clean_name(name):
+    return "".join([c for c in name if c.isalpha()])
 
 class ActionTopicSearch(Action):
+
+    def clean_name(name):
+        return "".join([c for c in name if c.isalpha()])
 
     def name(self) -> Text:
         return "topic_search_result"
@@ -17,6 +23,21 @@ class ActionTopicSearch(Action):
         
         topicget1 = tracker.get_slot("topicget1")
         dispatcher.utter_message(text=f"search topics are : {topicget1}")
-        dispatcher.utter_message(text="we can go through these topics!")
+        topic_list = topicget1.split(',')
+        for item in topic_list:
+            topic_search = clean_name(item)
+
+            # search in wiki #####
+            
+            info = wikipedia.summary(topic_search)
+            dispatcher.utter_message(topic_search)
+            dispatcher.utter_message(info)
+            
+            ## save into a file ###
+            with open(f"{topic_search}.txt","w") as file:
+                file.write(info)
+
+
+        dispatcher.utter_message(text="seach completed sussessfully!!!!")
 
         return[]

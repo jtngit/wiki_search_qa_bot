c1f1a4a0401ac3655367f85950c67cb95414c721
justine jose <justinejose@justines-MacBook-Air.local>
2022-04-11 16:19:46 +0530

    search topic added to view- some change , validate file some change

--- actions/search_topic.py
+++ actions/search_topic.py
@@ -3,15 +3,10 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet, AllSlotsReset
-import wikipedia
+import os
 
-def clean_name(name):
-    return "".join([c for c in name if c.isalpha()])
 
 class ActionTopicSearch(Action):
-
-    def clean_name(name):
-        return "".join([c for c in name if c.isalpha()])
 
     def name(self) -> Text:
         return "topic_search_result"
@@ -20,24 +15,27 @@ class ActionTopicSearch(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        
-        topicget1 = tracker.get_slot("topicget1")
-        dispatcher.utter_message(text=f"search topics are : {topicget1}")
-        topic_list = topicget1.split(',')
-        for item in topic_list:
-            topic_search = clean_name(item)
+            fo = open("list_of_topics.txt", "r+")
+            line = fo.read()
+            fo.close()
+            print (line)
+            search_topics_set= set(())
 
-            # search in wiki #####
+            if line == '':
+                dispatcher.utter_message(text="Nothing to search")
+                dispatcher.utter_message(text="Add topics type- 'ok go'")
+                return[]
+            else:
+                for a in line.split(','):
+                    if a != '' :
+                        search_topics_set.add(a)
+
+                for names in search_topics_set:
+                    dispatcher.utter_message(names)
+                    file = open(f"{names}.txt", "r+")
+                    lines = file.read()
+                    file.close()
+                    dispatcher.utter_message(lines)
             
-            info = wikipedia.summary(topic_search, auto_suggest=False)
-            dispatcher.utter_message(topic_search)
-            dispatcher.utter_message(info)
-            
-            ## save into a file ###
-            # with open(f"{topic_search}.txt","w") as file:
-            #     file.write(info)
 
-
-        dispatcher.utter_message(text="seach completed sussessfully!!!!")
-
-        return[]
+            return[]
